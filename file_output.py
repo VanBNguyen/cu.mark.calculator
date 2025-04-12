db2728ba3eff97d84910cab86af00a44e826cc29
Van Nguyen <van114.nguyen@gmail.com>
2025-04-11 20:24:19 -0400

    added main guard

--- file_output.py
+++ file_output.py
@@ -1,27 +1,31 @@
 from final_mark import percentages
 
-flag = True
+def main():
 
-marks_needed = {}
+    flag = True
 
-try:
-    num_courses = int(input("Number of courses: "))
-except Exception:
-    print("\n\nlol misinput TRY AGAIN\n\n")
+    marks_needed = {}
 
-for _ in range(num_courses):
-    course = input("Course Name: ")
-    course_mark = percentages()
-    marks_needed[course] = course_mark
+    try:
+        num_courses = int(input("Number of courses: "))
+    except Exception:
+        print("\n\nlol misinput TRY AGAIN\n\n")
 
-with open("marks_needed.txt", "w") as file:
-    for course in marks_needed:
-        file.write(f"{course}\n")
-        file.write(f"A+ --> {marks_needed[course][0]}%\n")
-        file.write(f"A  --> {marks_needed[course][1]}%\n")
-        file.write(f"A- --> {marks_needed[course][2]}%\n")
-        file.write("")
+    for _ in range(num_courses):
+        course = input("Course Name: ")
+        course_mark = percentages()
+        marks_needed[course] = course_mark
 
+    with open("marks_needed.txt", "w") as file:
+        for course in marks_needed:
+            file.write(f"{course}\n")
+            file.write(f"A+ --> {marks_needed[course][0]}%\n")
+            file.write(f"A  --> {marks_needed[course][1]}%\n")
+            file.write(f"A- --> {marks_needed[course][2]}%\n")
+            file.write("")
+
+if __name__ == "__main__":
+    main()
 
 
 
